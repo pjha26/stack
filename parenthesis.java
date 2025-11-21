c3c1c886adeb5c4dd51c48102a379a46adf9a1a8
pjha26 <praj80624@gmail.com>
2025-11-22 02:41:29 +0530

    parenthesis

---
+++ parenthesis.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class parenthesis {
+    public static boolean isValid(String str){
+        Stack<Character> s= new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            //opening
+            if(ch=='('||ch=='{'||ch=='['){
+                s.push(ch);
+            }else{
+                if(s.isEmpty()){
+                    return false;
+                }
+                if((s.peek()=='(' && ch==')') ||(s.peek()=='{' && ch=='}') || (s.peek()=='[' && ch==']')){
+                    s.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        if (s.isEmpty()) {
+            return true;
+            
+        }
+    }
+}
